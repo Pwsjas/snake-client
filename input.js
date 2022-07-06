b7061fd29b1e404e1ffe58d8de7a4d8879b8c045
Philip Simpson <pwsjas433@hotmail.com>
2022-07-06 03:33:26 +0000

    added movement functionality using wasd

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,6 +17,18 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     console.log('Exiting Game');
     process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
   }
 };
 
