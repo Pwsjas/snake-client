d4c1b906af1fcd80ef5517b83cf2dcb4306903a1
Philip Simpson <pwsjas433@hotmail.com>
2022-07-06 03:18:27 +0000

    added initial input framework

--- play.js
+++ play.js
@@ -1,5 +1,23 @@
 const net = require("net");
 const {connect} = require('./client');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    console.log('Exiting Game');
+    process.exit();
+  }
+};
 console.log("Connecting ...");
 connect();
+setupInput();
