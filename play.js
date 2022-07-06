2d74f1b34f588c399d5b3b34134e81c41be4c6ef
Philip Simpson <pwsjas433@hotmail.com>
2022-07-06 03:22:35 +0000

    moved input control to a seperate file, exported and imported accordingly

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
 const net = require("net");
 const {connect} = require('./client');
+const {setupInput} = require('./input');
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    console.log('Exiting Game');
-    process.exit();
-  }
-};
 console.log("Connecting ...");
 connect();
 setupInput();
